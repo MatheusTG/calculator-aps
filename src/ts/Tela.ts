f255b525ec6b38b4d443fde8898785ec023d5f92
Amanda Soares <130059710+AmandaSoaresV@users.noreply.github.com>
2024-10-03 11:45:51 -0300

    implementação

--- src/ts/Tela.ts
+++ src/ts/Tela.ts
@@ -1,3 +1,13 @@
-export class Tela {
-  constructor() {}
+import { DigitoTipo, TelaTipo } from "../@types/calculadora";
+
+export class Tela implements TelaTipo {
+  constructor() {
+    
+}
+  mostre(digito: DigitoTipo): void {
+    throw new Error("Method not implemented.");
+  }
+  limpe(): void { 
+    throw new Error("Method not implemented.");
+  }
 }
